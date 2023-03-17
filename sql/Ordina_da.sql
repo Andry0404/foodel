1995ead1c96fbf6909e1e9b5167049801ca9c834
Andry0404 <andreadegiorgi13@gmail.com>
2023-03-17 16:17:59 +0100

    Create tabelle sql per le relazioni

---
+++ sql/Ordina_da.sql
@@ -0,0 +1,14 @@
+drop table if exists Ordina_da;
+
+create table Ordina_da(
+
+    importo float(6,3) not null,
+    data_ordine date not null,
+    quantita int not null,
+
+ id_ristorante  int,
+ id_cliente int,
+    foreign key(id_ristorante) REFERENCES Ristorante (id_ristorante),
+    foreign key(id_cliente) REFERENCES Proprietario (id_cliente),
+    PRIMARY KEY(id_ristorante,id_cliente)
+);
---
+++ sql/Vende.sql
@@ -0,0 +1,8 @@
+drop table if exists Vende;
+
+create table Vende(
+    id_ristorante  int,
+    id_prodotto int,
+    foreign key(id_ristorante) REFERENCES Ristorante (id_ristorante),
+    foreign key(id_prodotto) REFERENCES Prodotto (id_prodotto)
+);
