288af626d66d68594cafb683d2e2740663c6f2ef
lifeofvins <vincenzo.sagristano@studenti.polito.it>
2023-03-17 16:39:16 +0100

    refactoring

--- sql/Generazione_tabelle/Scrive.sql
+++ sql/Generazione_tabelle/Scrive.sql
@@ -1,8 +1,8 @@
 drop table if exists Scrive;
 
 create table Scrive(
- id_recensione  int,
- id_cliente int,
+    id_recensione int,
+    id_cliente int,
     foreign key(id_recensione) REFERENCES Recensione (id_recensione),
     foreign key(id_cliente) REFERENCES Proprietario (id_cliente)
 );
--- sql/Generazione_tabelle/Vende.sql
+++ sql/Generazione_tabelle/Vende.sql
@@ -1,7 +1,7 @@
 drop table if exists Vende;
 
 create table Vende(
-    id_ristorante  int,
+    id_ristorante int,
     id_prodotto int,
     foreign key(id_ristorante) REFERENCES Ristorante (id_ristorante),
     foreign key(id_prodotto) REFERENCES Prodotto (id_prodotto)
