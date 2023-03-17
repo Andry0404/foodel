5dd31a38f6d6c6028cfdc1202d9238e9e776e21d
Andry0404 <andreadegiorgi13@gmail.com>
2023-03-17 16:37:57 +0100

    Aggiunto pizze

--- sql/Generazione_tabelle/Prodotto.sql
+++ sql/Generazione_tabelle/Prodotto.sql
@@ -2,7 +2,7 @@ drop table if EXISTS Prodotto;
 
 create table Prodotto(
   id_prodotto int primary key AUTO_INCREMENT,
-  prezzo varchar(255) not null,
+  prezzo float(6,3) not null,
   nome varchar(255) not null,
   allergeni varchar(255) not null,
   ingredienti varchar(255) not null
