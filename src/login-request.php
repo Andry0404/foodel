09dda106e3da2708e230213bd614204cbd78df63
Andry0404 <andreadegiorgi13@gmail.com>
2023-04-09 10:54:20 +0200

    cambio

---
+++ src/login-request.php
@@ -0,0 +1,22 @@
+<html>
+<head></head>
+<body>
+<?php
+
+$email=$_GET['email'];
+$password=$_GET['password'];
+
+$con=mysqli_connect("localhost","root","","foodelDB") or die ("Errore nella connessione al server mysql: " . mysqli_connect_error());
+
+$q="select ()";
+
+
+
+
+
+
+
+
+?>
+</body>
+</html>
