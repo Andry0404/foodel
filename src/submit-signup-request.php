9f3cda239cd00017d15542d0b8f4a34b4b0d97d5
Andry0404 <andreadegiorgi13@gmail.com>
2023-04-15 15:35:11 +0200

    modifica signup

--- src/submit-signup-request.php
+++ src/submit-signup-request.php
@@ -1,24 +1,32 @@
 <html>
-    <head></head>
-    <body>
-        <?php
-            $nome = $_GET['nome'];
-            $cognome = $_GET['cognome'];
-            $indirizzo = $_GET['indirizzo'];
-            $data_di_nascita = $_GET['data_di_nascita'];
-            $email = $_GET['email'];
-            $password = $_GET['password'];
 
-            $connection = mysqli_connect("localhost", "root", "", "foodelDB") or die ("ERROR: connection error with foodelDB. ".mysqli_connect_error());
+<head></head>
 
-            $query = "INSERT INTO Cliente(cognome, nome, indirizzo, email, data_nascita, hashed_password) "; 
-            $query = $query . "VALUES('$cognome', '$nome', '$indirizzo', '$email', '$data_di_nascita', '$password');";
+<body>
+    <?php
 
-            if(mysqli_query($connection, $query)) {
-                print("<h1>Inserzione eseguita!</h1>");
-            }
-            
-            mysqli_close($connection);
-        ?>
-    </body>
+    $nome = $_GET['nome'];
+    $cognome = $_GET['cognome'];
+    $indirizzo = $_GET['indirizzo'];
+    $data_di_nascita = $_GET['data_di_nascita'];
+    $email = $_GET['email'];
+    $password = $_GET['hashed_password'];
+
+    $connection = mysqli_connect("localhost", "root", "", "foodelDB") or die("ERROR: connection error with foodelDB. " . mysqli_connect_error());
+
+    $query = "INSERT INTO Cliente(cognome, nome, indirizzo, email, data_nascita, hashed_password) ";
+    $query = $query . "VALUES('$cognome', '$nome', '$indirizzo', '$email', '$data_di_nascita', '$password');";
+
+    $myquery = mysqli_query($connection, $query);
+
+    if ($myquery) {
+        print("<h1>Inserzione eseguita!</h1>");
+    } else {
+        print("<h1>Inserzione non eseguita!</h1>");
+    }
+
+    mysqli_close($connection);
+    ?>
+</body>
+
 </html>
