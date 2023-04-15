b2f7df2d50beac420f66ca2deb66e235eab336d9
lifeofvins <vincenzo.sagristano@studenti.polito.it>
2023-04-15 16:07:07 +0200

    aggiunto controllo validità campi

--- src/submit-signup-request.php
+++ src/submit-signup-request.php
@@ -12,20 +12,46 @@
     $email = $_GET['email'];
     $password = $_GET['password'];
 
-    $connection = mysqli_connect("localhost", "root", "", "foodelDB") or die("ERROR: connection error with foodelDB. " . mysqli_connect_error());
+    $campi_validi = true;
 
-    $query = "INSERT INTO Cliente(cognome, nome, indirizzo, email, data_nascita, hashed_password) ";
-    $query = $query . "VALUES('$cognome', '$nome', '$indirizzo', '$email', '$data_di_nascita', '$password');";
-
-    $myquery = mysqli_query($connection, $query);
-
-    if ($myquery) {
-        print("<h1>Inserzione eseguita!</h1>");
-    } else {
-        print("<h1>Inserzione non eseguita!</h1>");
+    if ($nome == "") {
+        $campi_validi = false;
+    }
+    if ($cognome == "") {
+        $campi_validi = false;
+    }
+    if ($indirizzo == "") {
+        $campi_validi = false;
+    }
+    if ($data_di_nascita == "") {
+        $campi_validi = false;
+    }
+    if ($email == "") {
+        $campi_validi = false;
+    }
+    if ($password == "") {
+        $campi_validi = false;
     }
 
-    mysqli_close($connection);
+    if ($campi_validi) {
+        $connection = mysqli_connect("localhost", "root", "", "foodelDB") or die("ERROR: connection error with foodelDB. " . mysqli_connect_error());
+
+        $query = "INSERT INTO Cliente(cognome, nome, indirizzo, email, data_nascita, hashed_password) ";
+        $query = $query . "VALUES('$cognome', '$nome', '$indirizzo', '$email', '$data_di_nascita', '$password');";
+
+        $myquery = mysqli_query($connection, $query);
+
+        if ($myquery) {
+            print("<h1>Inserzione eseguita!</h1>");
+        } else {
+            print("<h1>Inserzione non eseguita!</h1>");
+        }
+
+        mysqli_close($connection);
+    } else {
+        print("<h1>Non sono ammessi campi vuoti.</h1>")
+    }
+
     ?>
 </body>
 
