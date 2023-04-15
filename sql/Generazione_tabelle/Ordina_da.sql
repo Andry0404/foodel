b8773d416b4618c5ef00e7d59c3050dba0a394e3
lifeofvins <vincenzo.sagristano@studenti.polito.it>
2023-04-15 15:51:25 +0200

    sql fixed

--- sql/Generazione_tabelle/Ordina_da.sql
+++ sql/Generazione_tabelle/Ordina_da.sql
@@ -6,7 +6,7 @@ create table Ordina_da(
     quantita int not null,
     id_ristorante int,
     id_cliente int,
-    foreign key(id_ristorante) REFERENCES Ristorante (id_ristorante),
-    foreign key(id_cliente) REFERENCES Proprietario (id_cliente),
+    foreign key(id_ristorante) REFERENCES Ristorante(id_ristorante),
+    foreign key(id_cliente) REFERENCES Proprietario(id_cliente),
     PRIMARY KEY(id_ristorante, id_cliente)
 );
