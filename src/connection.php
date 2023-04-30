0869605819e1bc4514bd8ea408813e75b5a249f1
lifeofvins <vincenzo.sagristano@studenti.polito.it>
2023-04-30 09:45:24 +0200

    init: login

---
+++ src/connection.php
@@ -0,0 +1,11 @@
+<?php      
+    $host = "localhost";  
+    $user = "root";  
+    $password = '';  
+    $db_name = "foodelDB";  
+      
+    $con = mysqli_connect($host, $user, $password, $db_name);  
+    if(mysqli_connect_errno()) {  
+        die("Errore nella connessione al db: ". mysqli_connect_error());  
+    }
+?> 
