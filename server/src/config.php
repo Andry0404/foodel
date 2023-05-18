06dc402796e7f77fd053d0a4c10fea220c91ab2f
lifeofvins <vincenzo.sagristano@studenti.polito.it>
2023-05-18 17:11:05 +0200

    upd: config

--- server/src/config.php
+++ server/src/config.php
@@ -1,8 +1,7 @@
 <?php
    define('DB_SERVER', 'localhost');
-   define('DB_PORT', 3000);
    define('DB_USERNAME', 'root');
    define('DB_PASSWORD', '');
    define('DB_DATABASE', 'foodelDB');
-   $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE,DB_PORT);
+   $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
 ?>
