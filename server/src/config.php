63a2d95761b240dd35ffdcf480bf77ddc4c6681c
lifeofvins <vincenzo.sagristano@studenti.polito.it>
2023-05-18 16:48:40 +0200

    fix: mysql config

--- server/src/config.php
+++ server/src/config.php
@@ -1,5 +1,5 @@
 <?php
-   define('DB_SERVER', 'localhost:3000');
+   define('DB_SERVER', 'localhost');
    define('DB_USERNAME', 'root');
    define('DB_PASSWORD', '');
    define('DB_DATABASE', 'foodelDB');
